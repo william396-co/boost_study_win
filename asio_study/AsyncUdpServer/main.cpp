5cc913e49209c8d95f4ce9c0ae4da09dc5c4818f
huiping.liu <huiping.liu@silverkeyz.com>
2024-06-07 14:39:30 +0800

    fixed code

--- asio_study/AsyncUdpServer/main.cpp
+++ asio_study/AsyncUdpServer/main.cpp
@@ -49,14 +49,14 @@ private:
 
 int main(int argc, char* argv[]) {
 
-	auto port = 9527;
+	int16_t port = 9527;
 	if (argc > 1) {
 		port = std::strtol(argv[1], nullptr, 10);
 	}
 
 	try {
 		boost::asio::io_context io;
-		udp_server(io, port);
+		udp_server server{ io, port };
 		io.run();
 	}
 	catch (std::exception& e) {
