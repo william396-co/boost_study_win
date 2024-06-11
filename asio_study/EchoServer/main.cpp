709b668b72a3f58b9374ffda1035d7d84eaf548b
huiping.liu <huiping.liu@silverkeyz.com>
2024-06-11 17:09:37 +0800

    finished echo_server code

--- asio_study/EchoServer/main.cpp
+++ asio_study/EchoServer/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <memory>
+#include <chrono>
+#include <stdexcept>
 
 #include <boost/asio.hpp>
 #include <boost/asio/detached.hpp>
@@ -27,7 +29,33 @@ public:
 		boost::asio::spawn(strand_,
 			[this, self](boost::asio::yield_context yield) {
 
-			});
+				try {
+					char data[128];
+					while (true) {
+						timer_.expires_after(std::chrono::seconds{10});
+						std::size_t n = socket_.async_read_some(boost::asio::buffer(data), yield);
+						boost::asio::async_write(socket_, boost::asio::buffer(data, n), yield);
+					}
+				}
+				catch (std::exception& e) {
+					socket_.close();
+					timer_.cancel();
+				}
+
+			}, boost::asio::detached);
+
+		boost::asio::spawn(strand_,
+			[this, self](boost::asio::yield_context yield) {
+
+				while (socket_.is_open()) {
+					boost::system::error_code ignored_ec;
+					timer_.async_wait(yield[ignored_ec]);
+					if (timer_.expiry() <= boost::asio::steady_timer::clock_type::now()) {
+						socket_.close();
+					}
+				}
+			},
+			boost::asio::detached);
 	}
 private:
 	tcp::socket socket_;
@@ -36,7 +64,41 @@ private:
 };
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	try {
+
+		if (argc != 2) {
+			std::cerr << "Usage echo_server<port>\n";
+			return 1;
+		}
+
+		boost::asio::io_context io;
+		boost::asio::spawn(io,
+			[&](boost::asio::yield_context yeild) {
+				tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+
+				while (true) {
+					boost::system::error_code ec;
+					tcp::socket socket(io);
+					acceptor.async_accept(socket, yeild[ec]);
+					if (!ec) {
+						std::make_shared<session>(io, std::move(socket))->go();
+					}
+				}
+			},
+			[](std::exception_ptr e) {
+				if (e) {
+					std::rethrow_exception(e);
+				}
+			}
+		);
+		io.run();
+
+	}
+	catch (std::exception& e) {
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
 
 
 	return 0;
